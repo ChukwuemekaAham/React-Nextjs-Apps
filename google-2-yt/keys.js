ef531b03061b0fafeac5fd6a393ce550c62c3810
ChukwuemekaAham <ahamchukwuemeka2@gmail.com>
2024-08-19 10:27:49 +0100

    Added project

--- google-2-yt/keys.js
+++ google-2-yt/keys.js
@@ -1,11 +1,11 @@
 // API Key - https://developers.google.com/custom-search/v1/using_rest
 
-export const API_KEY = "AIzaSyATYu-RTD7WTbkMBlKMBGyoINUmoQgACIQ";
+export const API_KEY = "";
 
 // Context Key - https://cse.google.com/cse/create/new
 
 {
-  /* <script async src="https://cse.google.com/cse.js?cx=ccac24589e9d382a7"></script> */
+  /* <script async src="https://cse.google.com/cse.js?cx="></script> */
 }
 
-export const CONTEXT_KEY = "ccac24589e9d382a7";
+export const CONTEXT_KEY = "";
